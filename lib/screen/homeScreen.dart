e784b58e819b970a652b8f180aef0e7aa8c0a444
Georgefarah825 <162799743+Georgefarah825@users.noreply.github.com>
2024-03-17 20:38:38 +0300

    Update homeScreen.dart

--- lib/screen/homeScreen.dart
+++ lib/screen/homeScreen.dart
@@ -1,1 +1,23 @@
 
+import 'package:electronicstore/constants.dart';
+import 'package:electronicstore/widgets/home_body.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: kPrimaryColor,
+        title: Text('مرحبا بكم بمتجر الالكتروني'),
+        centerTitle: false,
+        actions: [IconButton(onPressed: null, icon: Icon(Icons.menu))],
+      ),
+      body: homeBody(),
+    );
+  }
+}
